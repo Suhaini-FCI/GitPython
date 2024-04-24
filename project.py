45edb827e166fbe3c21e2c2e9abfd6b01296a3ba
Su123 <suhaini.mmu@gmail.com>
2024-04-24 14:36:21 +0800

    add code to find multiplied value

--- project.py
+++ project.py
@@ -2,3 +2,5 @@ num1 = int (input("Enter first number: "))
 num2 = int (input ("Enter second number: "))
 sum = num1+num2
 print("Total is ", sum)
+product = num1*num2
+print("Multiplied value is ", product)
