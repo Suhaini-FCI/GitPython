cd68d1e6f4688a83c5c7600ce92b4c507bdc6336
Su123 <suhaini.mmu@gmail.com>
2024-04-24 14:40:28 +0800

    add if

--- project.py
+++ project.py
@@ -1,7 +1,10 @@
 num1 = int (input("Enter first number: "))
 num2 = int (input ("Enter second number: "))
 print ("Add or multiply?")
-sum = num1+num2
-print("Total is ", sum)
-product = num1*num2
-print("Multiplied value is ", product)
+ans = input("+ or *?")
+if ans =='+':
+    sum = num1+num2
+    print("Total is ", sum)
+else:
+    product = num1*num2
+    print("Multiplied value is ", product)
